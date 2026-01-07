b4a6f3d86f6e68c521adbe4ea2e4268462b7f494
Aearhart-Dominican <austin.earhart55@gmail.com>
2026-01-07 13:01:00 -0800

    UI button skeleton

---
+++ unit_button.gd
@@ -0,0 +1,12 @@
+extends TouchScreenButton
+
+@onready var progress: ProgressBar = $ProgressBar
+@onready var cooldown_timer: Timer = $Timer
+
+var unit : String
+var price : int
+var cooldown : float
+var icon : String
+
+func _process(_delta: float) -> void:
+	progress.value = cooldown_timer.time_left / cooldown_timer.wait_time
